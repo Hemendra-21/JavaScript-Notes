2608b162e2217530a1f0519c7008c9cc132abef6
hemendra <hemendra2k1@gmail.com>
2025-04-07 20:04:09 +0530

    added closures and this in regular and constructor functions

--- 12_Functions, Closures, THIS.js
+++ 12_Functions, Closures, THIS.js
@@ -1,3 +1,6 @@
+
+// Closures
+
 for (var i=0; i<=3; i++){
     setTimeout(()=>{
         console.log(i)
@@ -14,3 +17,123 @@ for (var i=0; i<=3; i++){
     }
     inner(i)
 }
+
+
+
+
+
+// ********** THIS ****************
+
+
+
+function details(){
+    return this
+}
+
+console.log(globalThis)
+
+console.log(details())
+
+const me = {
+    personName:'Hemendra',
+    details
+}
+
+
+console.log(me.details())
+
+
+
+// *********************** CALL and BIND*******************************
+
+// 💡 Real-life analogy:
+
+//     call() = like calling someone directly: “Hey, talk now.”
+
+//     bind() = like saving someone’s number and calling later.
+
+
+
+// bind
+
+function details(greeting){
+    console.log(`${greeting}, ${this.personName}`)
+}
+
+const name = {
+    personName : 'Hemendra'
+}
+
+const sayName = details.bind(name,'Hey')
+sayName()
+
+
+
+
+// call
+
+
+function details(greeting){
+    console.log(`${greeting}, ${this.personName}`)
+}
+
+const myName = {
+    personName : 'Hemendra'
+}
+
+details.call(myName,'Hello')
+
+
+
+
+// Constructor function
+
+function Person(name){
+    this.name = name,
+    this.details = function(){
+        console.log(this)
+    }
+}
+
+const mine = new Person('Hemendra')
+mine.details()
+
+
+
+
+
+
+function newDetails(name){
+    this.name = name
+
+    setTimeout(
+        function(){
+            console.log(this)
+        }.bind(this),100
+    )
+
+}
+
+
+const details1 = new newDetails('Hemendra')
+
+
+
+// ************************ //
+
+
+function newDetails(name){
+    this.name = name
+
+    setTimeout(
+        ()=>{
+            console.log(this)
+        },1000
+    )
+}
+
+
+const details2 = new newDetails('Hemendra')
+
+
+
